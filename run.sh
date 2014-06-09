a3b0e3b6bdaa976552f550e63cbaabfafe937898
V David Zvenyach <vdavez@gmail.com>
2014-06-09 17:54:15 -0400

    060914175409

--- run.sh
+++ run.sh
@@ -4,11 +4,5 @@ TIMESTAMP=$(date +%m%d%y%H%M%S)
 node app.js
 git checkout gh-pages
 git add .
-git reset -- etags.json
 git commit -am ${TIMESTAMP}
 git push origin gh-pages
-git checkout etags
-git reset etags
-git commit etags.json -m ${TIMESTAMP} 
-git push origin etags
-git checkout gh-pages
